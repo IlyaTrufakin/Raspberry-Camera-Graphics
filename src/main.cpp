a550765ceb9d7d801ecef310c73946a3d9f0b2a4
Fabricaferraria <Fabricaferraria@users.noreply.github.com>
2026-01-24 22:45:17 +0200

    main app argument - path to config

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,12 @@
 #include "app.h"
 
-int main() {
+#include <string>
+
+int main(int argc, char** argv) {
+    std::string config_path = "config.ini";
+    if (argc > 1 && argv[1] && argv[1][0] != '\0') {
+        config_path = argv[1];
+    }
     App app;
-    return app.run("config.ini");
+    return app.run(config_path);
 }
